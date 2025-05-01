aa60225474af36b920009a25f72da2e00640ca10
rambhakt1-star <rambhakthoonme@gmail.com>
2025-05-01 13:02:20 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -180,8 +180,8 @@ async def terms(client, message):
 @app.on_message(filters.command("plan") & filters.private)
 async def plan(client, message):
     plan_text = (
-        "> 💰 **Premium Price**:\n\n Starting from $2 or 200 INR accepted via **__Amazon Gift Card__** (terms and conditions apply).\n"
-        "📥 **Download Limit**: Users can download up to 100,000 files in a single batch command.\n"
+        "> 💰 **Premium Price**:\n\n Starting from 30rs for 1 month upi id - 9329548617@ybl __** (terms and conditions apply).\n"
+        "📥 **Download Limit**: Users can download up to 500 files in a single batch command.\n"
         "🛑 **Batch**: You will get two modes /bulk and /batch.\n"
         "   - Users are advised to wait for the process to automatically cancel before proceeding with any downloads or uploads.\n\n"
         "📜 **Terms and Conditions**: For further details and complete terms and conditions, please send /terms.\n"
